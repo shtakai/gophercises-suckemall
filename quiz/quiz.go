f79ca7b7eae1f438e8d624a70ee10add3f4277a9
Alyson Takai <shtakai@gmail.com>
2019-01-26 14:49:15 +0700

    Read csv from file

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -1,32 +1,58 @@
-package quiz
+package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
-	"strings"
+	"os"
 )
 
-func LoadCsv() {
-	quizes := []string {
-		"1+1, 2",
-		"2+1, 3",
+func main() {
+	filename := "problems.csv"
+	var numberOfQuestions = flag.Int("q", 3, "number of questions")
+	flag.Parse()
+	fmt.Println(filename)
+	fmt.Println(*numberOfQuestions)
+
+	var fp *os.File
+	fp, err := os.Open(filename)
+	if err != nil {
+		panic(err)
 	}
-	fmt.Printf("load file")
+	defer fp.Close()
 
-	csvString := strings.Join(quizes, "\n")
+	reader := csv.NewReader(fp)
+	reader.LazyQuotes = true
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		fmt.Println(record)
+	}
 
-    r := csv.NewReader(strings.NewReader(csvString))
-    for {
-        record, err := r.Read()
-        if err == io.EOF {
-            break
-        }
-        if err != nil {
-            fmt.Println("Read error: ", err)
-            break
-        }
-
-        fmt.Printf("%s = %s\n", record[0], record[1])
-    }
+	//quizes := []string{
+	//	"1+1, 2",
+	//	"2+1, 3",
+	//}
+	//fmt.Printf("load file\n")
+	//
+	//csvString := strings.Join(quizes, "\n")
+	//
+	//r := csv.NewReader(strings.NewReader(csvString))
+	//for {
+	//	record, err := r.Read()
+	//	if err == io.EOF {
+	//		break
+	//	}
+	//	if err != nil {
+	//		fmt.Println("Read error: ", err)
+	//		break
+	//	}
+	//
+	//	fmt.Printf("%s = %s\n", record[0], record[1])
+	//}
 }
