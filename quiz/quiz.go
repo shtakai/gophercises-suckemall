b580d22044c5082995a09097e2d5ffcb131edea5
Alyson Takai <shtakai@gmail.com>
2019-01-27 19:36:05 +0700

    Refactoring extract methods

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -21,8 +21,6 @@ func main() {
 	var numberOfQuestions = flag.Int("q", 3, "number of questions")
 	var filename = flag.String("f", "problems.csv", "question file")
 	flag.Parse()
-	fmt.Println(*filename)
-	fmt.Println(*numberOfQuestions)
 
 	var fp *os.File
 	fp, err := os.Open(*filename)
@@ -32,7 +30,31 @@ func main() {
 	defer fp.Close()
 
 	var qAndAs = make([]QAndA, 0)
+	qAndAs = createQAndAs(fp, qAndAs)
 
+	correct := performQAndA(numberOfQuestions, qAndAs)
+	fmt.Printf("Your fucked result is %v / %v\n", correct, *numberOfQuestions)
+}
+
+func performQAndA(numberOfQuestions *int, qAndAs []QAndA) int {
+	correct := 0
+	for i := 0; i < *numberOfQuestions; i++ {
+		stdInReader := bufio.NewReader(os.Stdin)
+		randomIndex := rand.Intn(len(qAndAs))
+		fmt.Printf("Question: %v = ?:", qAndAs[randomIndex].Question)
+		text, _ := stdInReader.ReadString('\n')
+		answer, _ := strconv.Atoi(strings.TrimSpace(text))
+		if answer == qAndAs[randomIndex].Answer {
+			fmt.Println("correct\n")
+			correct++
+		} else {
+			fmt.Println("fuck incorrect \n")
+		}
+	}
+	return correct
+}
+
+func createQAndAs(fp *os.File, qAndAs []QAndA) []QAndA {
 	reader := csv.NewReader(fp)
 	reader.LazyQuotes = true
 	for {
@@ -48,20 +70,5 @@ func main() {
 		}
 		qAndAs = append(qAndAs, QAndA{Question: record[0], Answer: answer})
 	}
-	stdInReader := bufio.NewReader(os.Stdin)
-	correct := 0
-	for i := 0; i < *numberOfQuestions; i++ {
-		randomIndex := rand.Intn(len(qAndAs))
-		fmt.Println(qAndAs[randomIndex])
-		fmt.Printf("Question: %v = ?:", qAndAs[randomIndex].Question)
-		text, _ := stdInReader.ReadString('\n')
-		answer, _ := strconv.Atoi(strings.TrimSpace(text))
-		if answer == qAndAs[randomIndex].Answer {
-			fmt.Println("correct\n")
-			correct++
-		} else {
-			fmt.Println("fuck incorrect \n")
-		}
-	}
-	fmt.Printf("Your fucked result is %v / %v", correct, *numberOfQuestions)
+	return qAndAs
 }
