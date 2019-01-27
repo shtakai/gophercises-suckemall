835aac95a29fbb75daa95ece5b03dd0c30ecb449
Alyson Takai <shtakai@gmail.com>
2019-01-27 19:31:51 +0700

    Add simple q and a input and result

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
 	"io"
+	"math/rand"
 	"os"
 	"strconv"
+	"strings"
 )
+
+type QAndA struct {
+	Question string
+	Answer   int
+}
 
 func main() {
 	var numberOfQuestions = flag.Int("q", 3, "number of questions")
 	var filename = flag.String("f", "problems.csv", "question file")
 	flag.Parse()
-	fmt.Println(filename)
+	fmt.Println(*filename)
 	fmt.Println(*numberOfQuestions)
 
 	var fp *os.File
@@ -23,7 +31,7 @@ func main() {
 	}
 	defer fp.Close()
 
-	qAndAs := make(map[string]int)
+	var qAndAs = make([]QAndA, 0)
 
 	reader := csv.NewReader(fp)
 	reader.LazyQuotes = true
@@ -38,9 +46,22 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		qAndAs[record[0]] = answer
+		qAndAs = append(qAndAs, QAndA{Question: record[0], Answer: answer})
 	}
+	stdInReader := bufio.NewReader(os.Stdin)
+	correct := 0
 	for i := 0; i < *numberOfQuestions; i++ {
+		randomIndex := rand.Intn(len(qAndAs))
+		fmt.Println(qAndAs[randomIndex])
+		fmt.Printf("Question: %v = ?:", qAndAs[randomIndex].Question)
+		text, _ := stdInReader.ReadString('\n')
+		answer, _ := strconv.Atoi(strings.TrimSpace(text))
+		if answer == qAndAs[randomIndex].Answer {
+			fmt.Println("correct\n")
+			correct++
+		} else {
+			fmt.Println("fuck incorrect \n")
+		}
 	}
-
+	fmt.Printf("Your fucked result is %v / %v", correct, *numberOfQuestions)
 }
