8db7ba04c7b12cbd54bc3f852e14c611402c32c6
Alyson Takai <shtakai@gmail.com>
2019-01-31 20:44:55 +0700

    Implement parsing json

--- adventure/cmd/web/main.go
+++ adventure/cmd/web/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/shtakai/gophercises-suckemall/adventure"
+)
 
 func main() {
-	fmt.Println("ho")
+	filename := flag.String("file", "gopher.json", "json w/story")
+	flag.Parse()
+
+	fmt.Printf("using story file: %v\n", *filename)
+	f, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	decoder := json.NewDecoder(f)
+	var story adventure.Story
+
+	if err := decoder.Decode(&story); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v", story)
 }
