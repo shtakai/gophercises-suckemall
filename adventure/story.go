b5b863c4a13870bf06f71a796932f975e3081017
Alyson Takai <shtakai@gmail.com>
2019-01-31 20:46:55 +0700

    cleanup

--- adventure/story.go
+++ adventure/story.go
@@ -14,34 +14,3 @@ type Option struct {
 }
 
 type Story map[string]Chapter
-
-//func main() {
-//	jsonStory := `
-//{"intro":
-//  {
-//    "title": "this is title",
-//    "story": [
-//      "something good story",
-//      "anything good story"
-//    ],
-//    "options": [
-//      {
-//        "text": "up",
-//        "arc": "up up"
-//      },
-//      {
-//        "text": "down",
-//        "arc": "down down"
-//      }
-//    ]
-//  }
-//}
-//
-//`
-//
-//	//err := json.Unmarshal([]byte(jsonStory), &Story)
-//	//if err != nil {
-//	//	fmt.Errorf("error %v", err)
-//	//}
-//	//fmt.Println(Story)
-//}
