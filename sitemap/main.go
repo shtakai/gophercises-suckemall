ac956ba6fc58efec481887a230cbe1cfe2413bf7
Alyson Takai <shtakai@gmail.com>
2019-04-21 15:17:30 +0700

    Add getting/ resp to STDOUT

--- sitemap/main.go
+++ sitemap/main.go
@@ -3,7 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
+
+/*
+	Strategy
+	1. get the web page
+	2. parse all links on the page
+	3. build proper url with our link
+	4. filter out w/ different url
+	5. find all pages w/ BFS
+	6. print out XML
+*/
 
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com/", "The root url for building a sitemap")
@@ -11,12 +24,11 @@ func main() {
 
 	fmt.Println(*urlFlag)
 
-	/*
-		1. get the web page
-		2. parse all links on the page
-		3. build proper url with our link
-		4. filter out w/ different url
-		5. find all pages w/ BFS
-		6. print out XML
-	*/
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	io.Copy(os.Stdout, resp.Body)
 }
